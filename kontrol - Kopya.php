8dd089227c204f3b4a721ac8def82c42cfeb494c
BNilay <ybelkisnilay@gmail.com>
2024-05-18 09:35:20 +0300

    son küçük düzeltmeler

--- kontrol - Kopya.php
+++ kontrol - Kopya.php
@@ -2,9 +2,7 @@
 if(isset($_POST['kullanici'],$_POST['sifre']) && ($_POST['kullanici']!='') && ($_POST['sifre']!='')){
     echo "Giriş başarılı";
 } else {
-    header("Location: giris.html");
-    exit();
-   // echo "Bilgilerinizi kontrol edin ve tekrar girin";
+    echo "Bilgilerinizi kontrol edin ve tekrar girin";
 }
 echo "<p><a href='giris.html'>&lt;GERİ DÖN;</a></p> ";
 ?>
